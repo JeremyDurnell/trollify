d0ae23fafbd35a763216991e37d37e61023ad13b
Mario Hoyos <mariohoyos92@gmail.com>
2018-03-08 18:22:02 -0600

    fix: stop propagation of play click

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -7,6 +7,7 @@ const RecommendationContainer = styled("div")`
   align-items: center;
   height: auto;
   width: 100%;
+  border-bottom: 3px solid #181818;
 `;
 
 const AlbumArt = styled("img")`
@@ -23,11 +24,30 @@ const SongInfo = styled("div")`
   flex-direction: column;
   justify-content: space-between;
 `;
-const Recommendation = ({ recommendation }) => (
+
+const TrackData = styled("span")`
+  font-weight: bold;
+  margin-right: 1em;
+`;
+
+const PlayButtonContainer = styled("div")`
+  width: 14%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SpotifyLink = styled("a")`
+  font-weight: bold;
+  text-decoration: none;
+  color: #1db954;
+  margin-top: 2em;
+`;
+const Recommendation = ({ recommendation, index, handleSelect, playing }) => (
   <RecommendationContainer>
     <AlbumArt
       src={
-        recommendation.album.images[0].url
+        recommendation.album.images[0]
           ? recommendation.album.images[0].url
           : "https://avatars1.githubusercontent.com/u/29843005?s=400&u=0fec472ecd3c19b61d7a9233b611e268e7cf8c98&v=4"
       }
@@ -35,22 +55,26 @@ const Recommendation = ({ recommendation }) => (
     />
     <SongInfo>
       <p>
-        <span>TRACK: </span>
+        <TrackData>Title: </TrackData>
         {recommendation.name}
       </p>
       <p>
-        <span>ARTIST:</span> {recommendation.artists[0].name}
+        <TrackData>Artist:</TrackData> {recommendation.artists[0].name}
       </p>
       <p>
-        <span>POPULARITY: </span>
+        <TrackData>Popularity: </TrackData>
         {recommendation.popularity}
       </p>
-    </SongInfo>
-    <div>
-      <a href={recommendation.external_urls.spotify} target="_blank">
+      <SpotifyLink href={recommendation.external_urls.spotify} target="_blank">
         Open in Spotify
-      </a>
-    </div>
+      </SpotifyLink>
+    </SongInfo>
+    <PlayButtonContainer onClick={e => handleSelect(index, e)}>
+      <i
+        className={playing ? "icon fa fa-pause" : "icon fa fa-play"}
+        style={{ fontSize: "3em" }}
+      />
+    </PlayButtonContainer>
   </RecommendationContainer>
 );
 
--- src/components/RecommendationPreview.js
+++ src/components/RecommendationPreview.js
@@ -15,20 +15,65 @@ export default class RecommendationPreview extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      activeMusic: ""
-    };
+    this.state = { activeMusicIndex: 0, play: false };
+
+    this.handleSelect = this.handleSelect.bind(this);
+    this._playMusic = this._playMusic.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
   }
 
+  handleSelect(index, event) {
+    event.stopPropagation();
+    this.state.activeMusicIndex === index
+      ? this.handleToggle()
+      : this._playMusic(index);
+  }
+
+  _playMusic(index) {
+    this.setState(
+      {
+        activeMusicIndex: index,
+        play: true
+      },
+      () => {
+        this.audioContainer.currentTime = 0;
+        this.audioContainer.play();
+      }
+    );
+  }
+
+  handleToggle() {
+    this.state.play ? this.audioContainer.pause() : this.audioContainer.play();
+    this.setState({ play: !this.state.play });
+  }
   render() {
+    console.log(this.props);
     const recommendations =
       this.props.recommendations.length > 1 &&
-      this.props.recommendations.map(recommendation => (
+      this.props.recommendations.map((recommendation, index) => (
         <Recommendation
           recommendation={recommendation}
           key={recommendation.name}
+          index={index}
+          handleSelect={this.handleSelect}
+          playing={this.state.activeMusicIndex === index && this.state.play}
         />
       ));
-    return <PreviewContainer>{recommendations}</PreviewContainer>;
+    const activeMusic =
+      this.props.recommendations.length > 1 &&
+      this.props.recommendations[this.state.activeMusicIndex];
+    return (
+      <PreviewContainer>
+        <audio
+          autoPlay={this.state.play}
+          preload="none"
+          ref={ref => {
+            this.audioContainer = ref;
+          }}
+          src={activeMusic.preview_url}
+        />
+        {recommendations}
+      </PreviewContainer>
+    );
   }
 }
